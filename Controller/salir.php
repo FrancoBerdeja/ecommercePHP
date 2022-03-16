df3794cf6b8f32560ad3d82a490e1bfd60ad0f48
FrancoBerdeja <98758860+FrancoBerdeja@users.noreply.github.com>
2022-03-16 11:46:15 -0300

    Add files via upload

--- Controller/salir.php
+++ Controller/salir.php
@@ -1,1 +1,8 @@
-
+<?php
+  session_start();
+  session_unset();
+  header("Location: ../");
+  $carrito = [];
+  file_put_contents('../Config/pcarrito.json', json_encode($carrito));
+  exit();
+?>
---
+++ Controller/validar.php
@@ -0,0 +1,37 @@
+<?php
+  include('../view/dbmlController.php');
+  include('../view/productoController.php');
+  if ((isset($_POST["usuario"])) && (isset($_POST["password"]))) {
+    $u = $_POST["usuario"];
+    $p = $_POST["password"];
+    $dbml = new dbManager("usuario","id_usuario");
+    $dbml->select();
+    $dbml->where('usuario','=',$u);
+    var_dump($dbml);
+    $usu = new obj($dbml->getArray());
+    var_dump($usu);
+    if (($usu->usuario = $u) && ($usu->contrasenia = $p)){
+      session_start();
+      $_SESSION["usuario"]=$u;
+      echo 'esta registrado';
+      if ($_SESSION["usuario"] == "admin") {
+        header("Location: ../Model/app/index.php");
+      }else{
+        header("Location: ../");
+      }
+    }else{
+      header("Location: ../view/login.php?error=c");
+    }
+  }  
+  // include("../dbml.php");
+  // $dbml = new dbManager("usuarios","id");
+  // $data = $dbml->buscarDbClave("firstname",$u);
+  // if ($data->num_rows == 1) {
+  //   session_start();
+  //   $_SESSION["usuario"] = $u;
+  //   $_SESSION["password"] = $p;
+  //   $dbml->close();
+  //   header("Location: ../index.php");
+  // }else{
+  //   header("Location: ../sections/login.php");
+  exit();
